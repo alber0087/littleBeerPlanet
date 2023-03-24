e8ddb185434de7ad72a2f76a0835b700ba5e8b17
Alber <albertorg0087@gmail.com>
2023-03-24 15:26:01 +0000

    move charecter left, right and jump

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const character = document.querySelector('.character')
-let bottom = 0
+document.addEventListener('DOMContentLoaded', () => {
+    const character = document.querySelector('.character')
+    let isJumping = false
+    let isGoingRight = false
+    let isGoingLeft = false
+    let bottom = 0
+    let gravity = 0.9
+    let left = 0
+    let leftTimerId
+    let rightTimerId
 
-function jump() {
-    bottom += 200
-    character.style.bottom = bottom + 'px'
-}
-
-function controls(e) {
-    switch(e.code) {
-        case 37:
-
-            break
-        case 39:
-
-            break
-        case 32 :
-            jump()
-            break
+    function jump() {
+        //jump up
+        isJumping = true
+        bottom += 30
+        bottom = bottom * gravity
+        character.style.bottom = bottom + 'px'
     }
-}
 
-document.addEventListener('keydown', controls)
+    function moveLeft() {
+            left -= 5
+            character.style.left = left + 'px'
+    }
+
+    function moveRight() {
+        left += 5    
+        character.style.left = left + 'px'
+            
+    }
+    
+
+    //assign functions to keycodes
+    function control(e) {
+        if (e.keyCode === 39) {
+            moveRight() //if we press the right arrow on our keyboard
+        } else if (e.keyCode === 32) {
+            jump() // if we press space
+        } else if (e.keyCode === 37){ 
+            moveLeft() // if we press left
+        }
+        console.log(e)
+    }
+    document.addEventListener('keyup', control)
+})
+
